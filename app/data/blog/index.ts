ea1a7234b40cb2e33ed236e9b105a44ff086f482
NEURON AI <neuronaicoin@gmail.com>
2026-06-17 22:09:28 +0300

    Add singaporeBwtsService to BLOG_POSTS

--- app/data/blog/index.ts
+++ app/data/blog/index.ts
@@ -72,8 +72,10 @@ import { istanbulBunkerSuppliers } from './bunker-suppliers-istanbul-2026-operat
 import { rotterdamShipRepair } from './ship-repair-services-rotterdam-2026-operator-buyer-guide';
 import { singaporeShipAgents } from './how-to-find-ship-agents-singapore-2026-operator-buyer-guide';
 import { singaporeShipchandlers } from './how-to-find-shipchandlers-singapore-2026-operator-buyer-guide';
+import { singaporeBwtsService } from './bwts-service-singapore-2026-operator-buyer-guide';
 export const BLOG_POSTS = [
   ...LEGACY_BLOG_POSTS,
+  singaporeBwtsService,
   singaporeShipchandlers,
   singaporeShipAgents,
   rotterdamShipRepair,
