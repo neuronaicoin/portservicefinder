254238cd9108165d0777553b6c6f2c88137fd1db
NEURON AI <neuronaicoin@gmail.com>
2026-09-07 07:50:09 +0300

    Add Singapore Registry of Ships guide import

--- app/data/blog/index.ts
+++ app/data/blog/index.ts
@@ -96,6 +96,7 @@ import { abuDhabiBunkerCrewChangeTankCleaning } from './bunker-supply-crew-chang
 import { majorWorldPortsTimeZonesScaleFacts } from './major-world-ports-time-zones-scale-operating-facts-2026';
 import { majorShippingCanalsChokepoints } from './major-shipping-canals-chokepoints-operator-guide-2026';
 import { publicHolidaysBusinessCultureByPortCountry } from './public-holidays-business-culture-major-port-countries-2026';
+import { singaporeRegistryOfShipsGuide } from './singapore-registry-of-ships-guide-ship-managers-2026';
 import { hongKongEngineRefrigerationElectrical } from './main-auxiliary-engine-refrigeration-electrical-automation-services-hong-kong-2026';
 import { tuzlaShipRepair } from './ship-repair-services-tuzla-istanbul-2026-operator-buyer-guide';
 import { bosphorusShipAgents } from './ship-agents-istanbul-bosphorus-2026-operator-buyer-guide';
@@ -285,6 +286,7 @@ export const BLOG_POSTS = [
   majorWorldPortsTimeZonesScaleFacts,
   majorShippingCanalsChokepoints,
   publicHolidaysBusinessCultureByPortCountry,
+  singaporeRegistryOfShipsGuide,
   hongKongEngineRefrigerationElectrical,
   singaporeEngineService,
   vesselTechnicalServiceVisits,
