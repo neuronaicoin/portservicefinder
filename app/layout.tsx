6186875daa7ceca4631e98b2652ea966d3316185
NEURON AI <neuronaicoin@gmail.com>
2026-09-07 18:54:57 +0300

    Integrate Facebook Pixel for tracking
    Added Facebook Pixel tracking script and noscript fallback to layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -134,6 +134,20 @@ export default function RootLayout({
             gtag('config', 'G-CFQV3SY7LX');
           `}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1048307384753465');
+            fbq('track', 'PageView');
+          `}
+        </Script>
         <Script
           id="schema-organization"
           type="application/ld+json"
@@ -153,7 +167,18 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(serviceSchema) }}
         />
       </head>
-      <body>{children}</body>
+      <body>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1048307384753465&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
